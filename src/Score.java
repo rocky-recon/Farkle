943a28972014ea49c078ecb0738532dbf01f3150
Daniel Lasche <dlasche@gmail.com>
2021-04-26 14:49:41 -0700

    score v4

--- src/Score.java
+++ src/Score.java
@@ -5,9 +5,9 @@ public class Score {
     //variables/fields
 
     //diceValues [], ArrayList?
-    int[] diceValues= {1, 1, 1, 1, 1, 1};
+    int[] CurrentDiceValues= {1, 1, 1, 1, 1, 1};
     int turnScore = 0;
-    ArrayList<ArrayList<Integer>> diceSets = new ArrayList<>();
+    ArrayList<ArrayList<Integer>> PreviousDiceSets = new ArrayList<>();
 
 
     //getters and setters
@@ -18,87 +18,109 @@ public class Score {
 
 
 
-    public int scoreCalculator(int[] diceValues){
+    public int scoreCalculator(int[] CurrentDiceValues){
         //if receiving an Arraylist, break into separate arrays (?)
         HashSet<Integer> setForSix = new HashSet<Integer>();
         HashSet<Integer> setForFive = new HashSet<Integer>();
 
-        if(diceValues.length == 1){
+        if(CurrentDiceValues.length == 1){
             //Array of One must be an Add On to a previous set of Three Dice, A One for 100, or a Five for 50.
-            for(ArrayList i:diceSets){
-                if(i.size() == 3){
-                    int j = (int) i.get(0);
-                    if( j == diceValues[0]){
-                        if(diceValues[0] == 1){
-                            turnScore+= 300;
-                        }else{
-                            turnScore+= diceValues[0]*100;
+            if(PreviousDiceSets.size()>0){
+                for(ArrayList i: PreviousDiceSets){
+                    if(i.size() == 3){
+                        int j = (int) i.get(0);
+                        if( j == CurrentDiceValues[0]){
+                            if(CurrentDiceValues[0] == 1){
+                                turnScore+= 300;
+                            }else{
+                                turnScore+= CurrentDiceValues[0]*100;
+                            }
+                        }else if(CurrentDiceValues[0]== 1){
+                            turnScore+= 100;
+                        }else if(CurrentDiceValues[0]== 5){
+                            turnScore+= 50;
                         }
-                    }else if(diceValues[0]== 1){
+                    }else if(CurrentDiceValues[0]== 1){
                         turnScore+= 100;
-                    }else if(diceValues[0]== 5){
+                    }else if(CurrentDiceValues[0]== 5){
                         turnScore+= 50;
+                    }else{
+                        return 0;
                     }
-                }else if(diceValues[0]== 1){
-                    turnScore+= 100;
-                }else if(diceValues[0]== 5){
-                    turnScore+= 50;
-                }else{
-                    return 0;
                 }
+            }else if(CurrentDiceValues[0]== 1){
+                turnScore+= 100;
+            }else if(CurrentDiceValues[0]== 5){
+                turnScore+= 50;
+            }else{
+                return 0;
             }
 
-        }else if(diceValues.length == 2){
-            //Array of Two must be an Add On to a previous set of Three Dice.
-            for(ArrayList i:diceSets) {
-                if (i.size() == 3) {
-                    int j = (int) i.get(0);
-                    if (j == diceValues[0]) {
-                        if (diceValues[0] == 1) {
-                            turnScore += 600;
-                        } else {
-                            turnScore += diceValues[0] * 2 * 100;
-                        }
-                    }else{
-                        for(Integer k:diceValues){
-                            if(k ==1){
-                                turnScore+=100;
-                            }else if(k==5){
-                                turnScore+=50;
+
+        }else if(CurrentDiceValues.length == 2){
+            //Array of Two must be an Add On to a previous set of Three Dice, or a combination of Ones and Fives.
+            if(PreviousDiceSets.size()>0){
+                for(ArrayList i: PreviousDiceSets) {
+                    if (i.size() == 3) {
+                        int j = (int) i.get(0);
+                        if (j == CurrentDiceValues[0]) {
+                            if (CurrentDiceValues[0] == 1) {
+                                turnScore += 600;
+                            } else {
+                                turnScore += CurrentDiceValues[0] * 2 * 100;
+                            }
+                        }else{
+                            for(Integer k:CurrentDiceValues){
+                                if(k ==1){
+                                    turnScore+=100;
+                                }else if(k==5){
+                                    turnScore+=50;
+                                }
                             }
                         }
+                    } else for(Integer y:CurrentDiceValues) {
+                        if (y == 1) {
+                            turnScore += 100;
+                        } else if (y == 5) {
+                            turnScore += 50;
+                        }
                     }
-                } else for(Integer y:diceValues) {
-                    if (y == 1) {
-                    turnScore += 100;
-                } else if (y == 5) {
-                    turnScore += 50;
+//                else {
+//                    return 0;
+//                }
                 }
-                }else {
-                    return 0;
+            }else{
+                for(Integer y:CurrentDiceValues) {
+                    if (y == 1) {
+                        turnScore += 100;
+                    } else if (y == 5) {
+                        turnScore += 50;
+                    }
                 }
             }
 
 
-        }else if(diceValues.length == 3){
+
+        }else if(CurrentDiceValues.length == 3){
             //Accounting for 3 Additional dice to a set of 3? Would need to check previous Array, see if dice values
             //were equal.
-                if(diceValues[0] == 1){
+            //need to account for Ones and Fives.
+                if(CurrentDiceValues[0] == 1){
                     turnScore+=300;
                 }else{
-                    turnScore+= diceValues[0]*100;
+                    turnScore+= CurrentDiceValues[0]*100;
                 }
 
-            }else if(diceValues.length == 4){
+            }else if(CurrentDiceValues.length == 4){
                 //four of a kind
                 turnScore += 1000;
 
-            }else if(diceValues.length == 5){
-                setForFive.add(diceValues[0]);
-                setForFive.add(diceValues[1]);
-                setForFive.add(diceValues[2]);
-                setForFive.add(diceValues[3]);
-                setForFive.add(diceValues[4]);
+            }else if(CurrentDiceValues.length == 5){
+                setForFive.add(CurrentDiceValues[0]);
+                setForFive.add(CurrentDiceValues[1]);
+                setForFive.add(CurrentDiceValues[2]);
+                setForFive.add(CurrentDiceValues[3]);
+                setForFive.add(CurrentDiceValues[4]);
 
                 if(setForFive.size() == 1){
                     //five of a kind
@@ -110,14 +132,14 @@ public class Score {
                 }
 
 
-            }else if(diceValues.length == 6){
+            }else if(CurrentDiceValues.length == 6){
                 //convert into hashset
-                setForSix.add(diceValues[0]);
-                setForSix.add(diceValues[1]);
-                setForSix.add(diceValues[2]);
-                setForSix.add(diceValues[3]);
-                setForSix.add(diceValues[4]);
-                setForSix.add(diceValues[5]);
+                setForSix.add(CurrentDiceValues[0]);
+                setForSix.add(CurrentDiceValues[1]);
+                setForSix.add(CurrentDiceValues[2]);
+                setForSix.add(CurrentDiceValues[3]);
+                setForSix.add(CurrentDiceValues[4]);
+                setForSix.add(CurrentDiceValues[5]);
 
                 if(setForSix.size() == 1){
                     //six of a kind
