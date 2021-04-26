15219260e7e750ba68081598e99e5e52f4525344
damianmercado32@gmail.com <61065466+rocky-recon@users.noreply.github.com>
2021-04-26 11:44:14 -0700

    updated Dice & Turn

--- src/Dice.java
+++ src/Dice.java
@@ -3,12 +3,15 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Dice {
+    private static final int MAX_NUMBER_SIDES = 6;
+    public static final Dice die = new Dice(MAX_NUMBER_SIDES);
     private String type;
     private Integer points;
-    private ArrayList<Integer> scoreVariant;
+    public ArrayList<Integer> diceFaceValue;
     private Integer faceValue;
     private SecureRandom dieSR = new SecureRandom();
     private boolean isSelected;
+
 
     public Dice() {
         int faceValue;
@@ -20,8 +23,12 @@ public class Dice {
         this.isSelected = isSelected;
     }
 
-    public void rollDice () {
+    public Dice(int maxNumberSides) {
+    }
+
+    public Integer rollDice () {
        Integer randomInteger = 1 + dieSR.nextInt(faceValue);
+       return randomInteger;
     }
 
     public Integer getFaceValue() {
@@ -36,10 +43,10 @@ public class Dice {
         isSelected = selected;
     }
     //    endTurn() return totalScore += turnScore; begin turn() for next player
-    public Dice(String type, Integer totalAmout, ArrayList<Integer> scoreVariant){
+    public Dice(String type, Integer totalAmout, ArrayList<Integer> diceFaceValue){
         this.type = type;
         this.points = totalAmout;
-        this.scoreVariant = scoreVariant;
+        this.diceFaceValue = diceFaceValue;
     }
 
 }
