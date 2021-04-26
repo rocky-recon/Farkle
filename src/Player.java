050e71d402d07577dd8c36774cb8c41e8b4036eb
Jonathan Urquieta <jonathan.urquieta@tlgcohort.com>
2021-04-26 14:38:03 -0600

    update to Farkle and Player class

--- src/Player.java
+++ src/Player.java
@@ -3,35 +3,63 @@ import java.util.Scanner;
 
 public class Player {
     public static void main(String[] args) {
-
-        players();
-        playerOne();
-
+        startGame();
     }
-    private static void players() {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter number of players ");
-        int name = in.nextInt();
-        System.out.println("Number of players are: " + name);
+    // this is called to have player enter game type, name, and score.
+    private static void startGame() {
+       Scanner in = new Scanner(System.in);
+        System.out.print("Select game type: 1: One player 2: Two players 3: Survival mode");
+        int gameType = in.nextInt();
+        if ( gameType == 1){
+            System.out.println("Game type is One Player");
+            onePlayer();
+            maxScore();
+        } else if (gameType == 2){
+            System.out.println("Game type is Two Players");
+            twoPlayers();
+            maxScore();
+        } else if (gameType == 3){
+            System.out.println("Game type is Survival mode");
+            onePlayer();
+            System.out.println("In survival mode you play until you get three Farkles");
+        }
     }
 
-
-
-    private static void playerOne() {
+    // if one player mode or survival is selected this is called
+    private static void onePlayer() {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter your name: ");
         String name = in.nextLine();
         System.out.println("Player name is: " + name);
-        in.close();
+    }
+    //if two player mode is selected this is called
+    private static void twoPlayers() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter first player's name: ");
+        String name = in.nextLine();
+        System.out.print("Enter second player's name: ");
+        String nameTwo = in.nextLine();
+        System.out.println("Players are : " + name + " and "+ nameTwo);
+    }
+    // player or players select score they must reach in order to win, survival mode has no score to reach in order to win.
+    private static void maxScore() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Select points need to win: 1: 5,000 2: 10,000 3: 15,000");
+        int score = in.nextInt();
+        if ( score == 1){
+            System.out.println("You selected 5,000");
+        } else if (score == 2){
+            System.out.println("You selected 10,000");
+        } else if (score == 3){
+            System.out.println("You selected 15,000");
+        }
+    }
+    private static void endGame() {
+
+
+
     }
 
-    private static void gameType() {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = in.nextLine();
-        System.out.println("Player name is: " + name);
-        in.close();
-    }
 }
 
 
