037ada8f13ba9f559c3f33dccfd676431ef497e5
damianmercado32@gmail.com <61065466+rocky-recon@users.noreply.github.com>
2021-04-27 15:39:18 -0700

    Damians changes and new dice

--- src/com/farkle/Dice.java
+++ src/com/farkle/Dice.java
@@ -2,65 +2,24 @@ package com.farkle;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 public class Dice {
-    ArrayList<Integer> currentDiceValues = new ArrayList<>();
-    private int[] faceValue;
+    public static void main(String[] args) {
+        ArrayList<Integer> RollingDice = new ArrayList<>();
+        Random die = new Random();
+        int die1 = die.nextInt(6);
+        int die2 = die.nextInt(6);
+        int die3 = die.nextInt(6);
+        int die4 = die.nextInt(6);
+        int die5 = die.nextInt(6);
+        int die6 = die.nextInt(6);
 
-    public static ArrayList<Integer> diceFaceValue = new ArrayList<>();
-
-    {
-        boolean allDiceHaveValues = false;
-        int die1;
-        int die2;
-        int die3;
-        int die4;
-        int die5;
-        int die6;
-
-        die1 = (int) (Math.random() * 6) + 1;
-        die2 = (int) (Math.random() * 6) + 1;
-        die3 = (int) (Math.random() * 6) + 1;
-        die4 = (int) (Math.random() * 6) + 1;
-        die5 = (int) (Math.random() * 6) + 1;
-        die6 = (int) (Math.random() * 6) + 1;
-        int[] faceValue = new int[]{die1, die2, die3, die4, die5, die6};
-        if (allDiceHaveValues) {
-            System.out.println("your dice values are " + faceValue);
-        } else if (!allDiceHaveValues) {
-            System.out.println("Please roll again");
-        }
-        ArrayList<Integer> diceFaceValue = new ArrayList<Integer>(faceValue.length);
-
-    }
-
-
-    public Dice(ArrayList<Integer> currentDiceValues, int[] faceValue) {
-        this.currentDiceValues = diceFaceValue;
-        this.faceValue = faceValue;
-    }
-
-    public ArrayList<Integer> getCurrentDiceValues() {
-        return currentDiceValues;
-    }
-
-    public void setCurrentDiceValues(ArrayList<Integer> currentDiceValues) {
-        currentDiceValues = diceFaceValue;
-    }
-
-    public int[] getFaceValue() {
-        return faceValue;
-    }
-
-    public void setFaceValue(int[] faceValue) {
-        this.faceValue = faceValue;
-    }
-
-    public static ArrayList<Integer> getDiceFaceValue() {
-        return diceFaceValue;
-    }
-
-    public static void setDiceFaceValue(ArrayList<Integer> diceFaceValue) {
-        Dice.diceFaceValue = diceFaceValue;
+        RollingDice.add(die1);
+        RollingDice.add(die2);
+        RollingDice.add(die3);
+        RollingDice.add(die4);
+        RollingDice.add(die5);
+        RollingDice.add(die5);
     }
 }
