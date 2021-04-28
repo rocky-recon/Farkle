2034ebeb46e8a5c463cb2ae6b55ecc668c5177b8
Damian Mercado <61065466+rocky-recon@users.noreply.github.com>
2021-04-27 19:49:51 -0700

    it works

--- src/com/farkle/Turn.java
+++ src/com/farkle/Turn.java
@@ -1,42 +1,37 @@
-//package com.farkle;
-//
-//import java.util.ArrayList;
-//import com.farkle.Score;
-//import com.farkle.Player;
-//
-//
-//public class Turn extends  Dice{
-//    public Turn(ArrayList<Integer> currentDiceValues, int[] faceValue) {
-//        super(currentDiceValues, faceValue);}
-//
-//    //    Roll Dice
-//    public static ArrayList<Integer> rollDice(Player player, int faceValue) {
-//        int MAX_ROLLS = 3;
-//        ArrayList<Integer> diceRoll = new ArrayList<Integer>();
-//        for (int roll = 0; roll < MAX_ROLLS; roll++){
-//            diceFaceValue.size();}
-//        return diceRoll;
-//    }
-//
-//    //    validating roll
-//    public static void validatingEachRoll(Player player, int faceValue) {
-//        ArrayList<Integer> diceRoll = rollDice(player, faceValue);
-//        validatingEachRoll(player, faceValue);
-//    }
-//
-//    //    Roll again
-//    public static void reRoll(Player player) {
-//        int turnScore;
-//        int MAX_NUM_DICES = 6;
-//        for (int rollingDice = 0; rollingDice < MAX_NUM_DICES; rollingDice++) {
-//            diceFaceValue.size();
-//        }
-//        if (MAX_NUM_DICES > 0) {
-//            diceFaceValue.size();
-//        } else if (MAX_NUM_DICES < 0) {
-//            // turnScore = scoreCalculator(scoreCalculator.values);
-//        }
-//        // player.resetMAX_NUM_DICES ();
-//        validatingEachRoll(player, MAX_NUM_DICES);
-//    }
-//}
+package com.farkle;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Turn {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> rollingDice = new ArrayList<>();
+        Random die = new Random();
+        int die1 = die.nextInt(6);
+        int die2 = die.nextInt(6);
+        int die3 = die.nextInt(6);
+        int die4 = die.nextInt(6);
+        int die5 = die.nextInt(6);
+        int die6 = die.nextInt(6);
+
+        rollingDice.add(die1);
+        rollingDice.add(die2);
+        rollingDice.add(die3);
+        rollingDice.add(die4);
+        rollingDice.add(die5);
+        rollingDice.add(die5);
+
+
+        ArrayList<Integer> turn = new ArrayList<>();
+    int MAX_ROLLS = 3;
+    boolean hasRoll = false;
+    for (int roll = 0; roll < MAX_ROLLS; roll++){
+          if(hasRoll = false)
+        System.out.println("Your Turn has ended. Please roll again " + roll);
+        else
+            System.out.println("Please roll again " + roll + rollingDice);}
+
+    }
+        }
+
