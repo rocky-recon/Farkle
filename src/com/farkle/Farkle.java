b442c3e1d3b58881c741edf7d7eea8204be4885a
Daniel Lasche <dlasche@gmail.com>
2021-04-28 11:17:18 -0700

    turnprintevents loops

--- src/com/farkle/Farkle.java
+++ src/com/farkle/Farkle.java
@@ -7,7 +7,7 @@ public class Farkle {
     public static void main(String[] args) {
         int totalScore;
         boolean farkle;
-        int totalFarkleCount;
+        //int totalFarkleCount;
         int rollsLeft;
         String playerName;
         Map<String, Integer> diceHeldPerTurn = new HashMap<String, Integer>();
