79f83fce4c4f7509973b688edf2e7d20b1fa7531
Damian Mercado <61065466+rocky-recon@users.noreply.github.com>
2021-04-28 07:49:50 -0700

    Update Turn.java

--- src/com/farkle/Turn.java
+++ src/com/farkle/Turn.java
@@ -6,13 +6,15 @@ import java.util.Random;
 public class Turn {
 
     public static void main(String[] args) {
-       ArrayList<Integer> turn = new ArrayList<>();
+      ArrayList<Integer> turn = new ArrayList<>();
         turn.add(1);
         turn.add(2);
         turn.add(3);
 
-        for (int counter = 0; counter < turn.size(); counter ++){
-            System.out.println(turn.get(counter));}
-
+//        for (int counter = 0; counter < turn.size(); counter ++){
+//            System.out.println(turn.get(counter));}
+        int roll = 0;
+        while (turn.size() > roll){
+            System.out.println(turn.get(roll));
+            roll++;
         }
-
