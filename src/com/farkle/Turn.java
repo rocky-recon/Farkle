8fdf724fd2de6413a473c0b95a7389a58da5d37c
Damian Mercado <61065466+rocky-recon@users.noreply.github.com>
2021-04-28 07:37:01 -0700

    Update Turn.java

--- src/com/farkle/Turn.java
+++ src/com/farkle/Turn.java
@@ -6,32 +6,13 @@ import java.util.Random;
 public class Turn {
 
     public static void main(String[] args) {
-        ArrayList<Integer> rollingDice = new ArrayList<>();
-        Random die = new Random();
-        int die1 = die.nextInt(6);
-        int die2 = die.nextInt(6);
-        int die3 = die.nextInt(6);
-        int die4 = die.nextInt(6);
-        int die5 = die.nextInt(6);
-        int die6 = die.nextInt(6);
+       ArrayList<Integer> turn = new ArrayList<>();
+        turn.add(1);
+        turn.add(2);
+        turn.add(3);
 
-        rollingDice.add(die1);
-        rollingDice.add(die2);
-        rollingDice.add(die3);
-        rollingDice.add(die4);
-        rollingDice.add(die5);
-        rollingDice.add(die5);
+        for (int counter = 0; counter < turn.size(); counter ++){
+            System.out.println(turn.get(counter));}
 
-
-        ArrayList<Integer> turn = new ArrayList<>();
-    int MAX_ROLLS = 3;
-    boolean hasRoll = false;
-    for (int roll = 0; roll < MAX_ROLLS; roll++){
-          if(hasRoll = false)
-        System.out.println("Your Turn has ended. Please roll again " + roll);
-        else
-            System.out.println("Please roll again " + roll + rollingDice);}
-
-    }
         }
 
